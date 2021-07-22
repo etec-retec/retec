9337987cf1ed3eb6b992c8da3e699eb4686a046a
leovasc5 <leovasc5@hotmail.com>
2021-07-21 22:22:46 -0300

    Iniciando tela de Projetos

--- projetos/index.php
+++ projetos/index.php
@@ -8,102 +8,26 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
         <link href="css/projetos.css" rel="stylesheet">
-        <title>Retec - Projeto</title>
+        <title>Retec - Projetos</title>
     </head>
     <body>
 
-    <div class="banner"> 
-    <!-- <div class="banner" backgroud-image: {{php}}> -->
+    <div class="cabecalho"> 
         <button class="voltar">❮ Voltar</button>
-        <!-- <img src="___php___"> -->
+        <h1 class="logo">RETEC</h1>
     </div>
 
     <div class="grid-container">
-
-    
+        <h2 class="fltrs">Filtros</h2>
         <div class="info_esquerda">
-            <div class="foto">
-                <img>
-            </div>
+            
 
-            <div class="caixa">
-                <p>
-                    <label><b>Prof. Orientador: </b></label> {{professor}}
-                </p>
-                <p>
-                    <label><b>Prof. Coorientador: </b></label> {{professor}}
-                </p>
-                <p>
-                    <label><b>Membros: </b></label>
-                    <ul>
-                        <li>{{Membro A}}</li>
-                        <li>{{Membro B}}</li>
-                        <li>{{Membro C}}</li>
-                        <li>{{Membro D}}</li>
-                    </ul>
-                </p>
-                <p>
-                    <label><b>Banca: </b></label>
-                    <ul>
-                        <li>{{Professor W}}</li>
-                        <li>{{Professor X}}</li>
-                        <li>{{Professor Y}}</li>
-                        <li>{{Professor Z}}</li>
-                    </ul>
-                </p>
-                <p>
-                    <label><b>Escola: </b></label> {{escola}}
-                </p>
-                <p>
-                    <label><b>Turma: </b></label> {{Turma}}
-                </p>
-                <p>
-                    <label><b>Data de Apresentação: </b></label> {{data}}
-                </p>
-                <p>
-                    <label><b>Ano de Conclusão: </b></label> {{ano}}
-                </p>
-                <p>
-                    <label><b>Menção: </b></label> {{mencao}}
-                </p>
-                <p>
-                    <label><b>Curso: </b></label> {{curso}}
-                </p>
-            </div>
-        <br><br>
+
         </div>
 
         <div class="info_direita">
 
-            <h2>Resumo</h2>
-            <div class="abstract">
-                
-            </div>
-
-            <h2>Abstract</h2>
-            <div class="abstract">
-
-            </div>
-
-            <h2>Palavras-chave</h2>
-            <div class="abstract">
-
-            </div>
-
-            <h2>Key Words</h2>
-            <div class="abstract">
-
-            </div>
-
-            <h2>Downloads</h2>
-            <div class="downloads">
-                <p>
-                    <label>Artigo: </label><input type="submit" value="PDF"/>
-                </p>
-                <p>    
-                    <label>Projeto Completo(.zip): </label><input type="submit" value="Baixar"/>
-                </p>
-            </div>
+            
         </div>
     </div>
     </body>
