b45b3486895d6132f0d24a8468581ad904c9e4f5
leovasc5 <leovasc5@hotmail.com>
2021-07-31 00:48:44 -0300

    Criando nova tela de Index

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="css/index.css" rel="stylesheet">
+        <title>Retec</title>
+    </head>
+    <body>
+        <div class="banner"> 
+            <div class="nome">
+                <h1>Retec</h1>
+            </div>
+            <div class="logo">
+                <img src="assets/img/logo.png" width="250px"/>
+            </div>
+        </div>
+
+        <div class="tec">
+            <img src="assets/img/bnr2.png"/>
+        </div>
+
+        <div class="sobre">
+            <div class="col1">
+
+            </div>
+            <div class="col2">
+
+            </div>
+            <div class="col3">
+
+            </div>
+        </div>
+    </body>
+</html>
